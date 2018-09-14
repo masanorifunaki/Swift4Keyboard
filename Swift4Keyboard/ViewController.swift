b638832290e149a5910501faba868f0521ace182
舟木正憲 <funakimfunaki@gmail.com>
2018-09-15 08:24:02 +0900

    first commit

--- Swift4Keyboard/ViewController.swift
+++ Swift4Keyboard/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var mailTextField: UITextField!
+    
+    
+    @IBOutlet var passwordTextField: UITextField!
+    
+    
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mailTextField.delegate = self
+        passwordTextField.delegate = self
+        
     }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        mailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        
+    }
+   
+    @IBAction func signIn(_ sender: Any) {
+        
+        resultLabel.text = mailTextField.text! + "" + passwordTextField.text!
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
